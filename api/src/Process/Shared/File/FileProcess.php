f609cc6be138febb1c4927d6eecedcbe990eb6a5
Rafael Escalera <escalerarodriguezr@gmail.com>
2021-11-03 09:29:54 +0100

    Unit test FileProcess

--- api/src/Process/Shared/File/FileProcess.php
+++ api/src/Process/Shared/File/FileProcess.php
@@ -4,21 +4,21 @@ namespace App\Process\Shared\File;
 
 use League\Flysystem\FilesystemOperator;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileProcess
 {
     const VISIBILITY_PUBLIC = 'public';
 
-    public function __construct(private FilesystemOperator $defaultStorage, private LoggerInterface $logger, private $projectDigitalOceanStoragePath)
+    public function __construct(private FilesystemOperator $defaultStorage, private LoggerInterface $logger, private string $projectDigitalOceanStoragePath)
     {
     }
 
-    /**
-     * @throws \League\Flysystem\FilesystemException
-     */
+
     public function uploadFile(UploadedFile $file, string $prefix, string $visibility): string
     {
+
         $fileName = \sprintf('%s%s/%s.%s', $this->projectDigitalOceanStoragePath,$prefix, \sha1(\uniqid()), $file->guessExtension());
 
         try{
@@ -29,13 +29,16 @@ class FileProcess
             );
 
         }catch (\Exception $e){
-            dd($e);
-
+            $this->logger->warning("Error uploading file");
+            throw new FileNotFoundException('Error uploading file');
         }
 
         return $fileName;
     }
 
+    /**
+     * @throws \League\Flysystem\FilesystemException
+     */
     public function deleteFile(?string $path): void
     {
         try {
@@ -44,6 +47,7 @@ class FileProcess
             }
         } catch (\Exception $e) {
             $this->logger->warning(\sprintf('File %s not found in the storage', $path));
+            throw new FileNotFoundException(\sprintf('File %s not found in the storage', $path));
         }
     }
 
