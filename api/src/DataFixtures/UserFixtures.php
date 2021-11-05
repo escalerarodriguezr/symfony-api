7112540b789a517ee252097aedacdf9d8b1d6dfe
Rafael Escalera <escalerarodriguezr@gmail.com>
2021-11-05 11:10:59 +0100

    Change in Fixtures

--- api/src/DataFixtures/UserFixtures.php
+++ api/src/DataFixtures/UserFixtures.php
@@ -17,6 +17,10 @@ class UserFixtures extends Fixture
     const FRODO_EMAIL = 'frodo@api.com';
     const FRODO_PASSWORD = 'frodo-password';
 
+    const NOT_EMAIL_CONFIRMED_NAME = 'Melkor';
+    const NOT_EMAIL_CONFIRMED_EMAIL = 'melkor@api.com';
+    const NOT_EMAIL_CONFIRMED_PASSWORD = 'melkor-password';
+
     public function __construct(private UserPasswordHasherInterface $passwordHasher)
     {
 
@@ -26,8 +30,10 @@ class UserFixtures extends Fixture
     {
         $peter = $this->createUser(self::PETER_NAME, self::PETER_EMAIL, self::PETER_PASSWORD);
         $frodo = $this->createUser(self::FRODO_NAME, self::FRODO_EMAIL, self::FRODO_PASSWORD);
+        $melkor = $this->createNotEmailConfirmedUser(self::NOT_EMAIL_CONFIRMED_NAME, self::NOT_EMAIL_CONFIRMED_EMAIL, self::NOT_EMAIL_CONFIRMED_PASSWORD);
         $manager->persist($peter);
         $manager->persist($frodo);
+        $manager->persist($melkor);
         $manager->flush();
     }
 
@@ -39,6 +45,20 @@ class UserFixtures extends Fixture
             $password
         );
         $user->setPassword($hashedPassword);
+        $user->setConfirmedEmail(true);
+        $user->setActive(true);
+        return $user;
+    }
+
+    private function createNotEmailConfirmedUser(string $name, string $email, string $password): User
+    {
+        $user = User::createUser($name, $email);
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $password
+        );
+        $user->setPassword($hashedPassword);
+
         return $user;
     }
 }
