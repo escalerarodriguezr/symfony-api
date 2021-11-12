f6ba704c28af6c8386512048427f2994fd10d1b9
Rafael Escalera <escalerarodriguezr@gmail.com>
2021-11-12 10:47:03 +0100

    Send mail form createUserService

--- api/src/Messenger/Handler/RegisterUserMessageHandler.php
+++ api/src/Messenger/Handler/RegisterUserMessageHandler.php
@@ -4,19 +4,36 @@ declare(strict_types=1);
 namespace App\Messenger\Handler;
 
 use App\Messenger\Message\User\RegisterUserMessage;
-use Psr\Log\LoggerInterface;
+use App\Service\Shared\Mailer\AppRoute;
+use App\Service\Shared\Mailer\MailerService;
+use App\Templating\TwigTemplate;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class RegisterUserMessageHandler implements MessageHandlerInterface
 {
 
-    public function __construct(private LoggerInterface $logger)
+    public function __construct(
+        private MailerService $mailerService,
+        private string $appHost
+    )
     {
 
     }
+
+    /**
+     * @throws \Twig\Error\SyntaxError
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\LoaderError
+     */
     public function __invoke(RegisterUserMessage $message): void
     {
-        $this->logger->info(\sprintf('Enviar a %s a su email: %s el code: %s', $message->getFullname(),$message->getEmail(), $message->getActivationCode()));
+        $payload = [
+            'name' => $message->getFullname(),
+            'url' => sprintf('%s%s/%s', $this->appHost, AppRoute::ACTIVATE_ACCOUNT, $message->getActivationCode())
+        ];
+
+        $this->mailerService->send($message->getEmail(),TwigTemplate::USER_REGISTER, $payload);
     }
 
 }
---
+++ api/src/Service/Shared/Mailer/AppRoute.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Shared\Mailer;
+
+abstract class AppRoute
+{
+    public const ACTIVATE_ACCOUNT = '/activate_account';
+
+}
