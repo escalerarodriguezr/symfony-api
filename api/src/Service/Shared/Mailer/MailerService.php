4d939b353e94386f6941c03f36d0f1f4cd87604b
Rafael Escalera <escalerarodriguezr@gmail.com>
2021-11-12 09:56:24 +0100

    Config templating

---
+++ api/src/Service/Shared/Mailer/MailerService.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Shared\Mailer;
+
+class MailerService
+{
+    public function __construct()
+    {
+    }
+
+    public function send(): void
+    {
+
+    }
+
+}
